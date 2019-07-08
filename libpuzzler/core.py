e581172faa47f46f20f1ee5e631d542c29899cfe
isilanes <isilanes@gmail.com>
2019-07-08 21:23:56 +0200

    Generate all unique Poses for a given Piece

--- libpuzzler/core.py
+++ libpuzzler/core.py
@@ -2,21 +2,43 @@
 import numpy as np
 
 
+# Functions:
+def reduced_form(np_array):
+    """Return input array with all-zero columns and rows removed."""
+
+    i_min, i_max = 0, 3
+    j_min, j_max = 0, 3
+
+    if np.array_equal(np_array[0], np.zeros((3,))):
+        i_min = 1
+
+    if np.array_equal(np_array[2], np.zeros((3,))):
+        i_max = 2
+
+    if np.array_equal(np_array[:, 0], np.zeros((3,))):
+        j_min = 1
+
+    if np.array_equal(np_array[:, 2], np.zeros((3,))):
+        j_max = 2
+
+    return np_array[i_min:i_max, j_min:j_max]
+
+
 # Classes:
 class Pose:
 
     # Constructor:
     def __init__(self, data=None):
         if data is None:
-            self._data = np.zeros((3, 3))
+            self.data = np.zeros((3, 3))
         else:
-            self._data = np.array(data)
+            self.data = np.array(data)
 
     # Public properties:
     def rotate(self, k=1):
         """Return the Pose, but rotated k*90 degrees CW."""
 
-        return Pose(data=np.rot90(self._data, k=k, axes=(1, 0)))
+        return Pose(data=np.rot90(self.data, k=k, axes=(1, 0)))
 
     def mirror(self):
         """Return mirror image of Pose, wrt plane Pose is contained in.
@@ -24,25 +46,46 @@ class Pose:
         xx. -> xx.
         xxx    ...
         """
-        return Pose(np.array([self._data[2, :], self._data[1, :], self._data[0, :]]))
+        return Pose(np.array([self.data[2, :], self.data[1, :], self.data[0, :]]))
 
     # Special methods:
     def __str__(self):
-        return str(self._data)
+        return str(self.data)
+
+    def __eq__(self, other):
+        return np.array_equal(reduced_form(self.data), reduced_form(other.data))
 
 
 class Piece:
 
     # Constructor:
-    def __init__(self, pose):
-        self._initial = pose
-        self._poses = [pose]
+    def __init__(self, initial=None):
+        self._initial = Pose(initial)
+        self.poses = []
 
     # Public methods:
     def generate_all_poses(self):
+        """Generator to return all possible poses."""
+
         for i_rot in range(4):
             yield self._initial.rotate(i_rot)
 
         for i_rot in range(4):
             yield self._initial.mirror().rotate(i_rot)
+
+    def generate_all_unique_poses(self):
+        """Take only poses that are not equal to existing ones."""
+
+        unique_poses = []
+        for new_pose in self.generate_all_poses():
+            already_taken_into_account = False
+            for old_pose in unique_poses:
+                if new_pose == old_pose:
+                    already_taken_into_account = True
+                    break
+
+            if not already_taken_into_account:
+                unique_poses.append(new_pose)
+
+        self.poses = unique_poses
 
--- puzzler.py
+++ puzzler.py
@@ -3,9 +3,31 @@ from libpuzzler import core
 
 
 # Main:
-pose = core.Pose([[0, 0, 0], [1, 1, 0], [1, 1, 1]])
-piece = core.Piece(pose)
+blue = core.Piece([[0, 0, 0],
+                   [1, 1, 0],
+                   [1, 1, 1]])
 
-for pose in piece.generate_all_poses():
-    print("")
+orange = core.Piece([[1, 0, 0],
+                     [1, 1, 1],
+                     [0, 1, 0]])
+
+purple = core.Piece([[0, 0, 0],
+                     [1, 1, 0],
+                     [1, 0, 0]])
+
+green = core.Piece([[0, 1, 1],
+                    [0, 0, 1],
+                    [0, 1, 1]])
+
+yellow = core.Piece([[1, 0, 0],
+                     [1, 1, 1],
+                     [1, 0, 0]])
+
+red = core.Piece([[0, 1, 1],
+                  [0, 0, 1],
+                  [0, 0, 1]])
+
+yellow.generate_all_unique_poses()
+for pose in yellow.poses:
+    print()
     print(pose)
